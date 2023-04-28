e2152ca6c86302dea2fdfe83d5cbff3f0001d70e
Saikat Das <102199984+iamsaikatdas@users.noreply.github.com>
2023-04-28 07:47:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //your JS code here. If required.
 let timer = document.getElementById('timer');
 let newDate =new Date();
+let date = new Date().toLocaleString();
 
-let fullDate = `${newDate.getFullYear} / ${newDate.getMonth(),} / ${newDate.getDate().}`
-let year = newDate.getFullYear()
-timer.innerText = year
+let fullDate = `${newDate.getDate()}/${newDate.getMonth()}/${newDate.getFullYear()}, `
+// let year = newDate.getFullYear()
+timer.innerText = date
