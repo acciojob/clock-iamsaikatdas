8d2c4794c95b5682bf640e40cb916a355499419c
Saikat Das <102199984+iamsaikatdas@users.noreply.github.com>
2023-04-28 07:43:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let timer = document.getElementById('timer');
 let newDate =new Date();
 
 let fullDate = `${newDate.getFullYear} / ${newDate.getMonth(),} / ${newDate.getDate().}`
-
-timer.innerText = fullDate
+let year = newDate.getFullYear()
+timer.innerText = year
